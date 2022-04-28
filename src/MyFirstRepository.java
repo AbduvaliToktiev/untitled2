260092ea1b2bd94c5d7f7a707f625614e5606ebd
toktiev72 <104427210+AbduvaliToktiev@users.noreply.github.com>
2022-04-28 16:08:51 +0600

    Delete src directory

--- src/MyFirstRepository.java
+++
@@ -1,5 +0,0 @@
-public class MyFirstRepository {
-    public static void main(String[] args) {
-
-    }
-}
--- src/month1/Praktik9.java
+++
@@ -1,7 +0,0 @@
-package month1;
-
-public class Praktik9 {
-    public static void main(String[] args) {
-
-    }
-}
